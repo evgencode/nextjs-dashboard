8d53246ffde46f296eef0a52b8aaaf64fd7154df
EvgeniyPetrov <epetrov@alfabank.ru>
2024-11-08 12:46:40 +0300

    chapter 11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,8 +16,8 @@ export const fetchRevenue = unstable_cache(
         // Artificially delay a response for demo purposes.
         // Don't do this in production :)
 
-        // console.log('Fetching revenue data...');
-        // await new Promise((resolve) => setTimeout(resolve, 2000));
+        console.log('Fetching revenue data...');
+        await new Promise((resolve) => setTimeout(resolve, 3000));
 
         const data = await sql<Revenue>`SELECT * FROM revenue ORDER BY month`;
 
