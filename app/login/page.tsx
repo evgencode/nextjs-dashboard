79ee0d46de732ab561fed68dd5cb6b3efabace02
EvgeniyPetrov <epetrov@alfabank.ru>
2024-11-17 19:25:18 +0300

    fixes 16

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,11 +4,15 @@ import LoginForm from '@/app/ui/login-form';
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             <AcmeLogo />
           </div>
+        </div>
+        <div className="flex flex-col max-w-[400px] p-4 bg-gray-50 rounded-lg">
+          <div>user@nextmail.com</div>
+          <div>123456</div>
         </div>
         <LoginForm />
       </div>
